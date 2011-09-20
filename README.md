1ffdd0727a08eee7aeda22c528bf93d9e38a276c
charliesome <charlie@charliesomerville.com>
2011-09-20 19:56:34 +1000

    readme, gitignore and html tweaks

---
+++ README.md
@@ -0,0 +1,16 @@
+# mandelbrot
+
+A Ruby C extension for rendering mandelbrots.
+
+## Compilation
+
+    ruby extconf.rb
+    make
+
+(yeah yeah, I'll make this into a proper gem later...)
+
+## Example
+
+![generation is super quick](http://i.imgur.com/7S4Bj.png)
+
+![the output is very pretty](http://i.imgur.com/tx6Ky.png)
--- html.rb
+++ html.rb
@@ -4,9 +4,9 @@ arr = Mandelbrot.new(Complex(-2, -1), Complex(1, 1)).compute(300, 200)
 
 puts "<style>* { margin:0px; padding:0px; }</style>"
 arr.each do |col|
-  puts "<div style='width:4px;float:left;'>"
+  puts "<div style='width:2px;float:left;'>"
   col.each do |cell|
-    puts "<div style='height:4px;width:4px;background:#{cell == Float::INFINITY ? "black" : "rgb(#{cell}, #{cell}, #{cell})"}'>&nbsp;</div>"
+    puts "<div style='height:2px;width:2px;background:#{cell == Float::INFINITY ? "black" : "rgb(#{cell}, #{cell}, #{cell})"}'>&nbsp;</div>"
   end
   puts "</div>"
 end
